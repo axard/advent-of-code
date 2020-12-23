beed81fbc11fc3accc931bd7e37b1eef2501f179
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-12-23 16:35:38 +0300

    Решил задачу №2 от 02.12.2015

--- src/bin/aoc2015-2.rs
+++ src/bin/aoc2015-2.rs
@@ -5,6 +5,7 @@ fn main() {
     let input = String::from_utf8_lossy(input);
 
     let mut full_area: i32 = 0;
+    let mut full_length: i32 = 0;
     for line in input.lines() {
         let values: Vec<&str> = line.split('x').collect();
         if values.len() == 3 {
@@ -12,11 +13,13 @@ fn main() {
             let w = values[1].parse::<i32>().unwrap();
             let h = values[2].parse::<i32>().unwrap();
 
-            full_area += area(l, w, h)
+            full_area += area(l, w, h);
+            full_length += length(l, w, h);
         }
     }
 
-    println!("{}", full_area)
+    println!("part 1: {} square feets", full_area);
+    println!("part 2: {} feets", full_length);
 }
 
 mod test {
@@ -32,17 +35,35 @@ mod test {
 }
 
 fn area(l: i32, w: i32, h: i32) -> i32 {
+    fn min_area(l: i32, w: i32, h: i32) -> i32 {
+        if max(l, max(w, h)) == h {
+            l * w
+        } else if max(l, max(w, h)) == w {
+            l * h
+        } else {
+            w * h
+        }
+    }
+
     let x: i32 = l * w + l * h + w * h;
-    let y: i32 = min(l, w, h);
+    let y: i32 = min_area(l, w, h);
     2 * x + y
 }
 
-fn min(l: i32, w: i32, h: i32) -> i32 {
+fn length(l: i32, w: i32, h: i32) -> i32 {
+    ribbon_length(l, w, h) + volume(l, w, h)
+}
+
+fn volume(l: i32, w: i32, h: i32) -> i32 {
+    l * w * h
+}
+
+fn ribbon_length(l: i32, w: i32, h: i32) -> i32 {
     if max(l, max(w, h)) == h {
-        l * w
+        2 * l + 2 * w
     } else if max(l, max(w, h)) == w {
-        l * h
+        2 * l + 2 * h
     } else {
-        w * h
+        2 * w + 2 * h
     }
 }
