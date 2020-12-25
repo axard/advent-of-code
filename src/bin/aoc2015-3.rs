9232b7dc3a292afafb44b473a5f2c1a6c2e5458d
Andrey Tarasov <andrey13.tarasov@yandex.ru>
2020-12-25 17:39:35 +0300

    Решил задачу №2 от 03.12.2015

--- src/bin/aoc2015-3.rs
+++ src/bin/aoc2015-3.rs
@@ -37,12 +37,12 @@ impl Moving {
     }
 }
 
-trait Move {
-    fn r#move(self, moving: Moving) -> Self;
+trait MoveTo {
+    fn move_to(self, moving: Moving) -> Self;
 }
 
-impl Move for Point {
-    fn r#move(self, moving: Moving) -> Self {
+impl MoveTo for Point {
+    fn move_to(self, moving: Moving) -> Self {
         match moving {
             Moving::Up => Point {
                 x: self.x,
@@ -64,31 +64,87 @@ impl Move for Point {
     }
 }
 
-fn parse(input: String) -> usize {
-    let mut point = Point { x: 0, y: 0 };
+struct Santa;
 
-    let mut hm = HashMap::new();
-    hm.insert(point, 1);
+impl Santa {
+    fn parse(input: String) -> usize {
+        let mut point = Point { x: 0, y: 0 };
 
-    for character in input.chars() {
-        let result = Moving::parse(character);
-        match result {
-            Ok(moving) => {
-                point = point.r#move(moving);
-                hm.insert(point, 1);
+        let mut hm = HashMap::new();
+        hm.insert(point, 1);
+
+        for character in input.chars() {
+            let result = Moving::parse(character);
+            match result {
+                Ok(moving) => {
+                    point = point.move_to(moving);
+                    hm.insert(point, 1);
+                }
+                Err(error) => println!("parse error: invalid character '{}'", error.character),
             }
-            Err(error) => println!("parse error: invalid character '{}'", error.character),
         }
-    }
 
-    hm.len()
+        hm.len()
+    }
+}
+
+struct SantaWithRobot;
+
+impl SantaWithRobot {
+    fn parse(input: String) -> usize {
+        let mut santa_point = Point { x: 0, y: 0 };
+        let mut robot_point = Point { x: 0, y: 0 };
+
+        let mut hm = HashMap::new();
+        hm.insert(santa_point, 1);
+        hm.insert(robot_point, 1);
+
+        let mut iter = input.chars();
+        loop {
+            if let Some(santa_command) = iter.next() {
+                let result = Moving::parse(santa_command);
+                match result {
+                    Ok(moving) => {
+                        santa_point = santa_point.move_to(moving);
+                        hm.insert(santa_point, 1);
+                    }
+                    Err(error) => println!("parse error: invalid character '{}'", error.character),
+                }
+
+                if let Some(robot_command) = iter.next() {
+                    let result = Moving::parse(robot_command);
+                    match result {
+                        Ok(moving) => {
+                            robot_point = robot_point.move_to(moving);
+                            hm.insert(robot_point, 1);
+                        }
+                        Err(error) => {
+                            println!("parse error: invalid character '{}'", error.character)
+                        }
+                    }
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+
+        hm.len()
+    }
 }
 
 fn main() {
     let input = String::from_utf8_lossy(include_bytes!("../../res/aoc2015/3/input.txt"));
-    let output = parse(input.into());
 
-    println!("part 1: {} houses with at least one present", output);
+    let santa_output = Santa::parse(input.clone().into());
+    println!("part 1: {} houses with at least one present", santa_output);
+
+    let santa_with_robot_output = SantaWithRobot::parse(input.clone().into());
+    println!(
+        "part 2: {} houses with at least one present",
+        santa_with_robot_output
+    );
 }
 
 #[cfg(test)]
@@ -97,41 +153,81 @@ mod tests {
 
     #[test]
     fn test_move_left() {
-        assert_eq!(parse("<".to_string()), 2);
+        assert_eq!(Santa::parse("<".to_string()), 2);
     }
 
     #[test]
     fn test_move_right() {
-        assert_eq!(parse(">".to_string()), 2)
+        assert_eq!(Santa::parse(">".to_string()), 2)
     }
 
     #[test]
     fn test_move_up() {
-        assert_eq!(parse("^".to_string()), 2)
+        assert_eq!(Santa::parse("^".to_string()), 2)
     }
 
     #[test]
     fn test_move_down() {
-        assert_eq!(parse("v".to_string()), 2)
+        assert_eq!(Santa::parse("v".to_string()), 2)
     }
 
     #[test]
     fn test_move_square() {
-        assert_eq!(parse("^>v<".to_string()), 4)
+        assert_eq!(Santa::parse("^>v<".to_string()), 4)
     }
 
     #[test]
     fn test_move_up_and_down() {
-        assert_eq!(parse("^v".to_string()), 2)
+        assert_eq!(Santa::parse("^v".to_string()), 2)
     }
 
     #[test]
     fn test_move_left_and_right() {
-        assert_eq!(parse("<>".to_string()), 2)
+        assert_eq!(Santa::parse("<>".to_string()), 2)
     }
 
     #[test]
     fn test_move_up_and_down_several_times() {
-        assert_eq!(parse("^v^v^v^v^v".to_string()), 2)
+        assert_eq!(Santa::parse("^v^v^v^v^v".to_string()), 2)
+    }
+
+    #[test]
+    fn test_move_left_with_robot() {
+        assert_eq!(SantaWithRobot::parse("<".to_string()), 2);
+    }
+
+    #[test]
+    fn test_move_right_with_robot() {
+        assert_eq!(SantaWithRobot::parse(">".to_string()), 2)
+    }
+
+    #[test]
+    fn test_move_up_with_robot() {
+        assert_eq!(SantaWithRobot::parse("^".to_string()), 2)
+    }
+
+    #[test]
+    fn test_move_down_with_robot() {
+        assert_eq!(SantaWithRobot::parse("v".to_string()), 2)
+    }
+
+    #[test]
+    fn test_move_square_with_robot() {
+        assert_eq!(SantaWithRobot::parse("^>v<".to_string()), 3)
+    }
+
+    #[test]
+    fn test_move_up_and_down_with_robot() {
+        assert_eq!(SantaWithRobot::parse("^v".to_string()), 3)
+    }
+
+    #[test]
+    fn test_move_left_and_right_with_robot() {
+        assert_eq!(SantaWithRobot::parse("<>".to_string()), 3)
+    }
+
+    #[test]
+    fn test_move_up_and_down_several_times_with_robot() {
+        assert_eq!(SantaWithRobot::parse("^v^v^v^v^v".to_string()), 11)
     }
 }
